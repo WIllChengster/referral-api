ad846e9bd057659eaa053a21e59eb7dbff4f8dd1
burninggun <willchengsters@gmail.com>
2020-06-15 17:14:10 -0700

    added clicks and conversion amount endpoints

--- app.js
+++ app.js
@@ -8,14 +8,52 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/create_link', (req, res) => {
-    const { user_id } = req.body;
+    const { user_id, redirect_url } = req.body;
     var sql = `
-        INSERT INTO links (user_id)
-        VALUES( ${db.escape(user_id)} )
+        INSERT INTO links (user_id, redirect_url)
+        VALUES( ${db.escape(user_id)}, ${db.escape(redirect_url)} )
     `
     db.query(sql, (error, results, fields) => {
-        console.log (error, results, fields);
+        if(err){
+            res.status(400).send(error)
+        }
+
+        res.send(results);
     })
+})
+
+app.get('/referral/:user_id', (req, res) => {
+    const { user_id } = req.params;
+    
+    var update_sql = `
+        UPDATE links
+        SET clicks = clicks + 1
+        WHERE USER_ID = ?
+    `
+    var select_sql = `
+        SELECT * FROM links
+        WHERE USER_ID = ?
+        LIMIT 1
+    `
+    db.query(update_sql, [user_id], (error, insertResults) => {
+        db.query(select_sql, [user_id], (error, selectResults) => {
+            res.redirect(selectResults[0].redirect_url);
+        });
+    });
+});
+
+app.get('/referral/conversion/:user_id', (req, res) => {
+    const { user_id } = req.params;
+
+    var update_sql = `
+        UPDATE links
+        SET conversions = conversions + 1
+        WHERE USER_ID = ?
+    `
+    db.query(update_sql, [user_id], (error, insertResults) => {
+        res.status(300).end();
+    })
+
 })
 
 app.listen(PORT, () => {
