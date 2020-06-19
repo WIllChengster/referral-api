a287532ad9c1d5e464b1c30407f9efd09bb55e82
burninggun <willchengsters@gmail.com>
2020-06-18 20:45:05 -0700

    promises for creating links

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config({path: './.env'});
+const dotenv = require('dotenv').config({ path: './.env' });
 const db = require('./database')
 const app = express();
 
@@ -13,37 +13,50 @@ app.post('/create_link', (req, res) => {
     var select_sql = `
         SELECT * FROM links where USER_ID = ?
     `
-    db.query(select_sql, [user_id], (error, results) => {
-        if(error){
-            res.status(400).send(error);
-            console.error(error);
-        }
-        if(!!results[0]){
-            res.send(JSON.stringify(results[0].redirect_url))
-            
-        } else {
 
-            var insert_sql = `
+
+    const query = async () => {
+        let queryPromise = new Promise((resolve, reject) => {
+            db.query(select_sql, [user_id], (error, results) => {
+                if (error) {
+                    console.error(error);
+                    reject(false);
+                }
+                if (results[0]) {
+                    resolve(results[0].redirect_url)
+                } else {
+                    resolve(false);
+                }
+            })
+        })
+        let result = await queryPromise;
+
+        var insert_sql = `
                 INSERT INTO links (user_id, redirect_url, referral_url)
                 VALUES( ${db.escape(user_id)}, ${db.escape(redirect_url)}, 
-                        ${process.env.DOMAIN}/referral/${db.escape(user_id)} )
+                        ${db.escape(process.env.DOMAIN + '/referral/' + user_id)} )
             `
+        if (!result) {
             db.query(insert_sql, (error, results, fields) => {
-                if(error){
+                if (error) {
                     res.status(400).send(error)
                     console.error(error);
                 }
                 console.log(results);
                 res.send(redirect_url);
             })
+        } else {
+            res.send(query)
         }
-    })
+
+    }
+    query();
 
 })
 
 app.get('/referral/:user_id', (req, res) => {
     const { user_id } = req.params;
-    
+
     var update_sql = `
         UPDATE links
         SET clicks = clicks + 1
@@ -56,7 +69,7 @@ app.get('/referral/:user_id', (req, res) => {
     `
     db.query(update_sql, [user_id], (error, insertResults) => {
         db.query(select_sql, [user_id], (error, selectResults) => {
-            if(!selectResults[0]){
+            if (!selectResults[0]) {
                 res.send('Link not found!')
             }
             res.redirect(selectResults[0].redirect_url);
