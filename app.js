c08734bff63a9e8425af9bc12931f7845c09ab1a
burninggun <willchengsters@gmail.com>
2020-06-18 20:00:58 -0700

    error handling if query not available in redirect

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ app.get('/referral/:user_id', (req, res) => {
     `
     db.query(update_sql, [user_id], (error, insertResults) => {
         db.query(select_sql, [user_id], (error, selectResults) => {
-            if(error){
-                console.error(error);
+            if(!selectResults[0]){
+                res.send('Link not found!')
             }
             res.redirect(selectResults[0].redirect_url);
         });
@@ -73,7 +73,6 @@ app.get('/referral/conversion/:user_id', (req, res) => {
         WHERE USER_ID = ?
     `
     db.query(update_sql, [user_id], (error, insertResults) => {
-        console.error(error);
         res.status(300).end();
     })
 
