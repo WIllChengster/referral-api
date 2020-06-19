d142bf3e808303bed32f2f5b4d2ab056337b5450
burninggun <willchengsters@gmail.com>
2020-06-18 20:56:01 -0700

    incorporating ssl

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv').config({path: './.env'});
-
+const fs = require('fs');
 const pool  = mysql.createPool({
     connectionLimit : 10,
     host            : process.env.DB_HOST,
     user            : process.env.DB_USER,
     password        : process.env.DB_PASSWORD,
-    database        : process.env.DB_DATABASE
+    database        : process.env.DB_DATABASE,
+    ssl  : {
+        ca : fs.readFileSync(__dirname + '/mysql-ca.crt')
+      }
 });
 
 
